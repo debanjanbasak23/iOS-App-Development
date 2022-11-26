0b5f6cc88effba5466d25aa3dbdf8a932c04c6b7
Debanjan Basak <92038706+debanjanbasak23@users.noreply.github.com>
2022-11-26 13:50:37 +0530

    Add files via upload

---
+++ Day-4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("hello, world")
+print("greeting")
+print(greeting)
+
+let cityname = "san fransisco"
+var srini = "hai"
+let a = srini
+print(a)
